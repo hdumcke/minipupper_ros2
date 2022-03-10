3a2ccd603090e1bfd9917c8bf264aa22a5593eba
Horst Dumcke <hdumcke@marlysys.com>
2022-03-10 10:03:46 +0100

    disable Mangdang services

--- install_robot.sh
+++ install_robot.sh
@@ -9,6 +9,10 @@ sudo adduser $USER input
 ### Install required Python packages
 sudo pip install msgpack transforms3d
 
+### Disable Mangdang services
+sudo systemctl disable robot
+sudo systemctl disable joystick
+
 ### Install ROS2
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
